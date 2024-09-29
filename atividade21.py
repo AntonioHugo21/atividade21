2e9adde853bb5f504d16b1527dea8d3139f4ea33
AntonioHugo21 <antonio.pierre@aluno.ce.gov.br>
2024-09-29 12:01:09 -0300

    contagem regressiva

---
+++ atividade21.py
@@ -0,0 +1,8 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final.
+
+quant_numeros = 10
+
+for cont in range (0,quant_numeros):
+    print(quant_numeros - cont)
+
+print('FIM!')
